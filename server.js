d5f8bb0d76ea3a1e4eb9515ff82abb1c085bd8ec
samera farah <samerafarah2@gmail.com>
2021-07-23 09:20:33 +0100

    files created

---
+++ server.js
@@ -0,0 +1,79 @@
+const express = require ('express');
+const app = express ();
+
+const mongoClient = require('mongodb').MongoClient
+let db;
+
+mongoClient.connect('mongodb+srv://samera:farah@cluster0.tlcth.mongodb.net/'
+, (err, client) => {
+db = client.db('webstore')
+})
+
+app.param('collectionName'
+, (req, res, next, collectionName) => {
+req.collection = db.collection(collectionName)
+return next()
+})
+
+app.get('/'
+, (req, res, next) => {res.send('Select a collection, e.g., /collection/CollectionName')
+})
+
+
+app.get('/collection/:collectionName'
+, (req, res, next) => {
+req.collection.find({}, {limit: 5, sort: [['price'
+         , +1]]})
+    .toArray((e, results) => {
+         if (e) return next(e)
+       res.send(results) }) })
+
+app.post('/collection/:collectionName'
+, (req, res, next) => {
+req.collection.insert(req.body, (e, results) => {
+if (e) return next(e)
+res.send(results.ops)}) })
+
+
+const ObjectID = require('mongodb').ObjectId;
+app.get('/collection/:collectionName/:id',(req,res,next) => {
+req.collection.findone(
+      {_id: new ObjectID(req.params.id) },
+                (e, result) => {
+           if (e) return next(e)
+              res.send(result) }) })
+
+
+
+
+app.put('/collection/:collectionName/:id'
+, (req, res, next) => {
+req.collection.update(
+{ _id: new ObjectID(req.params.id) },
+{ $set: req.body },
+{ safe: true, multi: false },
+(e, result) => {
+if (e) return next(e)
+res.send((result.result.n === 1) ?
+{msg: 'success'} : { msg: 'error'})
+})
+})
+
+
+app.delete('/collection/:collectionName/:id'
+, (req, res, next) => {
+req.collection.deleteOne(
+{ _id: ObjectID(req.params.id) },
+(e, result) => {
+if (e) return next(e)
+res.send((result.result.n === 1) ?
+{msg: 'success'} : {msg: 'error'})
+})
+})
+
+app.listen(3000, ()=> {
+console.log('express is working on server localhost:3000')})
+
+
+
+
